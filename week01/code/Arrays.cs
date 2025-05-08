a9037229bedaf16a1cf16623ab3bbd0de69bb47b
Shayla Foley <shaylafoley@Mac.lan>
2025-05-08 16:21:43 -0400

    Initial Commit

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Step one: Create a new doubles array, it's "length" will be the size
+        //Step two: Use a for loop to get the array data
+        //Step three: Start with index being 0 to the length -1 (because we want it to be the length number and the array will start at 0)
+        //Step four: The result will be multiply the number by the place it is in the length
+        //Step five: Return the array after the loop is done
+
+        double[] result = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i+1);
+        }
+        return result; 
     }
 
     /// <summary>
@@ -29,5 +40,22 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Step one: Determine the count of the data and assign it to a variable
+        //Step two: Split the data into two parts
+        //Step three: Clear the old list
+        //Step four: Join the two lists
+
+        int count = data.Count;
+
+        List<int> lastPart = data.GetRange(count-amount, amount);
+
+        List<int> firstPart = data.GetRange(0, count-amount);
+
+        data.Clear();
+
+        data.AddRange(lastPart);
+        data.AddRange(firstPart);
+
     }
 }
